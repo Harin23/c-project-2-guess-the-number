462e5a7aaeb0c468a752c5ac9b7481d8d38eac17
Harin23 <63739311+Harin23@users.noreply.github.com>
2026-06-27 18:46:27 -0600

    allow seven guesses

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 
+int ALLOWED_ATTEMPTS = 7;
+
 int main(){
     while(1) {
+        int random = 60;
+        int guess;
+        for (int i=0; i<ALLOWED_ATTEMPTS; i++) {
+            printf("Provide a guess for integer between 1-100 inclusive:\n");
+            scanf("%d", &guess);
 
+            if (guess > random) printf("Too high\n");
+            else if (guess < random) printf("Too low\n");
+            else break;
+        }
 
+        if (guess == random) printf("You win!\n");
+        else printf("You lose! Correct answer was %d\n", random);
 
         char choice;
         printf("Play again? (y/n)\n");
